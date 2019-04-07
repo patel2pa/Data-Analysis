8ae481d71b6868c3e31074bb6787471fde8943a1
Parth Patel <parthpp1996@gmail.com>
2019-04-07 19:00:03 -0400

    Update Stock_data_analysis.py

--- Data_Analysis/Stock_data_analysis.py
+++ Data_Analysis/Stock_data_analysis.py
@@ -5,6 +5,10 @@ saved the data into pandas,
 used three machine learning algorithm 
 to analye the data
 '''
+''
+The end goal for this algo is to use postive or negative relationship between two or more companies to predict prices of the stock
+'''
+
 import requests
 import bs4 as bs
 import pandas as pd
@@ -16,179 +20,174 @@ from sklearn.ensemble import VotingClassifier, RandomForestClassifier
 from sklearn.model_selection import train_test_split
 import numpy as np
 
-pd.set_option('display.expand_frame_repr', False)
-
-def get_stocks():
-    list_of_stock = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
-    soup = bs.BeautifulSoup(list_of_stock.text)
-    tab = (soup.find('table', {'class':'wikitable sortable'}))
-    tab_1 = tab.find_all('td')
-    contant = []
-    for row in tab_1:
-        if row.get_text().upper() and len(row.get_text())<=5:
-            contant.append(row.get_text())
-    
-    
-    new_contant = [x.replace('\n','') for x in contant]
-    
-    
-    return new_contant
 
 
-def get_stock_data():
-    datas = get_stocks()
-    if not os.path.exists('s_and_p_500'):
-        os.makedirs('s_and_p_500')
 
-    start = dt.datetime(2018, 1, 1)
-    end = dt.datetime(2019,1, 22)
-    
-    for data in datas:
+def s_and_p_list():
+  
+    main_df = pd.DataFrame()    
+    for data in os.listdir('s_and_p_500/'):
+        new_main_df = pd.DataFrame()
+        df = pd.read_csv('s_and_p_500/{}'.format(data))
+##        main_df['{}_volume'.format(data)] = df['Volume']
+##        main_df['{}_prices'.format(data)] = df['Adj Close']
+        new_main_df['{}_open'.format(data)] = df['High']
+        new_main_df['{}_close'.format(data)] = df['Low']
+        main_df['{}_avg'.format(data)] =  new_main_df.mean(axis = 1)
+##        print(new_main_df)
         
-        if not os.path.exists('s_and_p_500/{}.csv'.format(data)):
-            try: 
-            
-                df = web.DataReader(data, 'yahoo', start, end)
-                df.to_csv('s_and_p_500/{}.csv'.format(data))
-                
+    main_df.drop(main_df.index[[-1]], inplace = True)
+    
+    
+    return main_df 
+     
+##s_and_p_list() 
+    
+def per_stock(stock):
+    df = pd.DataFrame()
+    new_df = pd.DataFrame()
+    my_stock = pd.read_csv('s_and_p_500/{}.csv'.format(stock))
+    df['open'] = my_stock['High']
+    df['close'] = my_stock['Low']
+    new_df['avg'] = df.mean(axis = 1)
+      
+    return (new_df)
+
+##per_stock('AAPL')
+
+
+
+def comparison(stock):
+    stock_corr = []
+    
+    stocks = s_and_p_list()
+    my_stock = per_stock(stock)
+    stocks['avg'] = my_stock['avg']
+    
+    stocks['avg'] = stocks['avg'].shift(-1)
+    stocks.drop(stocks.index[[-1]], inplace = True)
+    stooo = pd.DataFrame()
+##    stooo['stock'] = stocks['{}.csv_open'.format(stock)]
+    stocks.drop(['{}.csv_avg'.format(stock)], axis=1, inplace = True)
+    
+  
+    df_new = stocks[[ticker for ticker in stocks.columns.values.tolist()]].pct_change()
+    
+
+    df_new_new = pd.DataFrame()
+    for st in df_new.columns:
+        df_new_new[st] = df_new[st]
+        for num in range(len(df_new[st])):
+            try:     
+                if (df_new[st][num]) > 0.0 :
+                    df_new_new.at[num, st] = 1
+                else:
+                    df_new_new.at[num, st] = -1
             except Exception:
                 pass
-        else:
-            print('got it')
-    
-get_stock_data()  
-    
-def calc():
-    datas = get_stocks()
-    
-    main_df = pd.DataFrame()
-    try: 
-        for data in datas:
-            df = pd.read_csv('stock_mid_data/{}.csv'.format(data))
-            main_df['{}_volume'.format(data)] = df['Volume']
-            main_df['{}_prices'.format(data)] = df['Adj Close']
-            main_df['{}_open'.format(data)] = df['Open']
-    
-    except Exception:
-        pass
-    return main_df 
+                                               
+    for stocksss in df_new_new.columns:
+        stock_corr.append((df_new_new[stocksss].corr(df_new_new['avg'])))
+    second_stock_corr = {}    
+    for stocksss in df_new_new.columns:
+        corrr = df_new_new[stocksss].corr(df_new_new['avg'])
+        second_stock_corr[stocksss] = corrr
+    strong_stock_corr = {}
 
+    for st in second_stock_corr:
+        if   (second_stock_corr[st]) > .16:
+            strong_stock_corr[st] = second_stock_corr[st]
+    strong_stock_corr.pop('avg')
 
-def corr(stock_name):
+    strong_stock_table = pd.DataFrame()
 
-    list_of_stock = calc()
+    for sts in strong_stock_corr:
+        strong_stock_table[sts] = df_new_new[sts]
     
-    main_df = pd.DataFrame()
-    main_df['{}_prices'.format(stock_name)] = list_of_stock['{}_prices'.format(stock_name)]
-    main_df['{}_volume'.format(stock_name)] = list_of_stock['{}_volume'.format(stock_name)]
-    main_df['{}_open'.format(stock_name)] = list_of_stock['{}_open'.format(stock_name)]
-    return main_df
+    
+    strong_stock_table['avg'] = df_new_new['avg']
+    
+    (strong_stock_table.drop(strong_stock_table.index[[0]],inplace = True))
+    
+'''
+testing the programm data
+'''
+##    print(second_stock_corr)
+##    print(strong_stock_table)
 
+    return (strong_stock_table)  
+                
+##    print(max_count)
+##    print(min_count)
+##    print(max_value)
+##    print(min_value)
 
-def precent_change(stock_name):
-    df = corr(stock_name)
-    
-##  df_new = pd.DataFrame(columns = ['prices_precent_change', 'volume_precent_change'])
-    
-    
-    #AAPL_prices  AAPL_volume
-   
-##
-##            precent_change = ((df.AAPL_prices[i+1]-df.AAPL_prices[i])/df.AAPL_prices[i])*100
-##            df_new = df_new.append({'prices_precent_change': precent_change }, ignore_index=True)
-##
-##            precent_change_vol = ((df.AAPL_volume[i+1]-df.AAPL_volume[i])/df.AAPL_volume[i])*100
-##            df_new = df_new.append({'volume_precent_change': precent_change_vol }, ignore_index=True)
+##(comparison('AAPL'))
 
-    df_new = df[[ticker for ticker in df.columns.values.tolist()]].pct_change()
-    df_new['prices_precent_change'] = df_new['{}_open'.format(stock_name)]
-    df_new['volume_precent_change'] = df_new['{}_volume'.format(stock_name)]
-    df_new['close_price_precent_change'] = df_new['{}_prices'.format(stock_name)]
+def machine_stuff(stock):
+    stock_list = comparison(stock)
     
+##    df_new = stock_list[[ticker for ticker in stock_list.columns.values.tolist()]].pct_change()
     
-    df_new.drop(['{}_open'.format(stock_name), '{}_volume'.format(stock_name),'{}_prices'.format(stock_name) ], axis = 1, inplace = True)
-    df_new_new = pd.DataFrame()##machining_the_data('AAPL')
-
-    df_new_new['label'] = df_new['prices_precent_change']
-    df_new_new['volume_label'] =  df_new['prices_precent_change']
-   
-    for tic in range(len(df['{}_prices'.format(stock_name)])):
-       
-        
-            
-        if (df['{}_prices'.format(stock_name)][tic]) > (df['{}_open'.format(stock_name)][tic]):
-            df_new_new.at[tic, 'label'] = 1
-            ##the .at() method replaces the row value with new value
-        elif (df['{}_prices'.format(stock_name)][tic]) < (df['{}_open'.format(stock_name)][tic]):
-            df_new_new.at[tic, 'label'] = -1 
-        else:
-            df_new_new.at[tic, 'label'] = 0
+    #main_df.drop(main_df.index[[-1]], inplace = True)
+    df_new = stock_list
+##    (df_new.drop(df_new.index[[0]],inplace = True))
     
-            
-    df_new['label'] = df_new_new.label
-    (df_new['open'])=df['{}_open'.format(stock_name)]
-    
-    
-    df_new.drop([('prices_precent_change'),('close_price_precent_change'), ('volume_precent_change')], axis = 1, inplace = True )
-    
-##    df_new.volume_precent_change[0] = 0
-    list_of_label = (list(df_new['label']))
-    count_1 = list_of_label.count(-1)
-    count_2 = list_of_label.count(0)
-    count_3 = list_of_label.count(1)
-    
+##    df_new_new = pd.DataFrame()
+##    
+##    df_new_new['labels'] = df_new['avg']
+##    
+##    
+##    for num in range(len(df_new['avg'])):
+##        try:
+##        
+##            if (df_new['avg'][num]) > 0.0 :
+##                df_new_new.at[num, 'labels'] = 1
+##            else:
+##                df_new_new.at[num, 'labels'] = -1
+##        except Exception:
+##            pass
+##   
+##    (df_new['labels']) = df_new_new['labels']
+##    (df_new['labels'][df_new.index[-1]]) = 1
+##    
 ##    print(df_new)
-##    print('the count of -1:', count_1)
-##    print(10*'-')
-##    print('the count of 0:', count_2)
-##    print(10*'-')
-##    print('the count of 1:', count_3)
-##    print(df)
+##    df_new.drop('avg', axis = 1, inplace = True)
+        
+##    print(df_new['{}.csv_open'.format(stock)], df_new['labels'])
     
-    stock_avg_colu = (df['{}_volume'.format(stock_name)].mean())
-    for vol in range(len(df['{}_volume'.format(stock_name)])):
+##    x = np.array(df_new['{}'.format(col)])
+##    X = x.reshape(-1,1)
+        
+        
+    y = np.array(df_new['avg'])
 
-
-        if (df['{}_volume'.format(stock_name)][vol]) > stock_avg_colu:
-            df_new_new.at[vol, 'volume_label'] = 1
-
-
-        elif (df['{}_volume'.format(stock_name)][vol]) < stock_avg_colu:
-            df_new_new.at[vol, 'volume_label'] = -1
-
-
-        else:
-            df_new_new.at[vol, 'volume_label'] = 0
-
-
-    df_new['volume_label'] = df_new_new['volume_label']
     
-   
-    print(df_new)    
-    return df_new
-#precent_change('AAPL')
-    
-def machining_the_data(stock):
-    stock_data = precent_change(stock)
-    print(stock_data)
-    X = np.array(stock_data.drop(['label'],1))
-    y = np.array(stock_data['label'])
-    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = .50)
-
+    X = np.array(df_new.drop(['avg'], 1))
+        
+    x_train, x_test, y_train, y_test = train_test_split(X,y, test_size = .75)
+        
     clf = VotingClassifier([('lsvc',svm.LinearSVC()),
-                            ('knn',neighbors.KNeighborsClassifier()),
-                            ('rfor',RandomForestClassifier())])
-
-    clf.fit(X_train, y_train)
-    confidence = clf.score(X_test, y_test)
-    print('accuracy:',confidence)
-##    print(X_test)
-    ## the first column is the precent change in volume, second is the open and third is the volume labels
+                                ('knn',neighbors.KNeighborsClassifier()),
+                                ('rfor',RandomForestClassifier())])
+        
+    clf.fit(x_train, y_train)
+        
+    confidence = clf.score(x_test, y_test)
     
-    predictions = clf.predict(np.array([[ 34, 1.00000000]]))
-   
-    print(predictions)
-     
-    
+        
+    print(confidence)
+        
+    return confidence
 
-##machining_the_data('AAPL')
+(machine_stuff('AAPL'))
+
+'''
+Analyzing the results, by getting 
+30 different data points 
+'''
+y = []
+    
+for r in range(30):
+    if r < 25:
+        y.append((machine_stuff('F')))
